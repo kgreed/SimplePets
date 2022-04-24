c4060996a0f53ff159224d63e0124721e32dea92
Kirsten Greed <kirsteng@jobtalk.com.au>
2022-04-25 05:14:09 +1000

    2 Actions

---
+++ SimplePets.Module.Win/Controllers/Helpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Configuration;
+using Microsoft.EntityFrameworkCore;
+using SimplePets.Module.BusinessObjects;
+namespace SimplePets.Module.Win.Controllers
+{
+    public static class Helpers
+    {
+        
+        public static SimplePetsEFCoreDbContext MakeDb()
+        {
+            var connectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+            var optionsBuilder = new DbContextOptionsBuilder<SimplePetsEFCoreDbContext>()
+                .UseSqlServer(connectionString);
+            return new SimplePetsEFCoreDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ SimplePets.Module.Win/Controllers/KittenViewController.cs
@@ -0,0 +1,69 @@
+﻿using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.Actions;
+using SimplePets.Module.BusinessObjects;
+using System;
+using System.Linq;
+
+namespace SimplePets.Module.Win.Controllers
+{
+    public class KittenViewController : ViewController
+    {
+        SimpleAction actionAddKittenEF;
+        SimpleAction actAddKittenXAF;
+        public KittenViewController() : base()
+        {
+            TargetObjectType = typeof(Kitten);
+            TargetViewNesting = Nesting.Nested;
+            actAddKittenXAF = new SimpleAction(this, "Add via OS", "View");
+            actAddKittenXAF.Execute += actAddKittenXAF_Execute;
+
+            actionAddKittenEF = new SimpleAction(this, "Add via Db", "View");
+            actionAddKittenEF.Execute += actionAddKittenEF_Execute;
+
+        }
+        private void actionAddKittenEF_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+            var cat = View.ObjectSpace.GetObject(((NestedFrame)Frame).ViewItem.CurrentObject) as Cat;
+            var db = Helpers.MakeDb();
+            var kitten = new Kitten
+            {
+                Parent = db.Cats.FirstOrDefault(c => c.Id == cat.Id),
+                Name = $"baby {cat.Kittens.Count + 1} of {cat.Name}"
+            };
+            db.Kittens.Add(kitten);
+            db.SaveChanges();
+            View.ObjectSpace.Refresh();
+
+
+
+        }
+        private void actAddKittenXAF_Execute(object sender, SimpleActionExecuteEventArgs e)
+        {
+
+            var cat = View.ObjectSpace.GetObject(((NestedFrame)Frame).ViewItem.CurrentObject) as Cat;
+            var os = View.ObjectSpace;
+            var kitten = os.CreateObject<Kitten>();
+            kitten.Parent = cat;
+            kitten.Name = $"baby {cat.Kittens.Count + 1} of {cat.Name}";
+            View.ObjectSpace.CommitChanges();
+            View.ObjectSpace.Refresh();
+
+        }
+        protected override void OnActivated()
+        {
+            base.OnActivated();
+            // Perform various tasks depending on the target View.
+        }
+        protected override void OnDeactivated()
+        {
+            // Unsubscribe from previously subscribed events and release other references and resources.
+            base.OnDeactivated();
+        }
+        protected override void OnViewControlsCreated()
+        {
+            base.OnViewControlsCreated();
+            // Access and customize the target View control.
+        }
+    }
+
+}
